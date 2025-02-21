46bfe0f4f73d9dd5f3be7f45b9c832fb7dfa04b6
bilgekosee <bilge.koosee@gmail.com>
2025-02-21 13:50:56 +0300

    ejs ye çevrildi ve page aktiflik durumu kontrol edildi

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require("express");
 
 const app = express();
 
+//template engine
+app.set("view engine", "ejs");
+
+//middlewears
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("INDEX SAYFASI");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+});
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
 });
 
 const port = 3000;
