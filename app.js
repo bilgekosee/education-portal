db95a8620faa5bffe64a0452f4a20200831898a8
bilgekosee <bilge.koosee@gmail.com>
2025-02-27 12:00:12 +0300

    kurs modeli oluşturuldu

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
 
 const app = express();
 
+//connect db
+mongoose
+  .connect("mongodb://localhost/smartedu-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB connected success");
+  });
 //template engine
 app.set("view engine", "ejs");
 
 //middlewears
 app.use(express.static("public"));
 
+//routes
 app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
