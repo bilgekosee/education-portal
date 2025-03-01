9a2bb256a0bbb1d54d671f3809801b923be88633
bilgekosee <bilge.koosee@gmail.com>
2025-03-01 22:11:08 +0300

    kurslar listelendi

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.set("view engine", "ejs");
 
 //middlewears
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //routes
 app.use("/", pageRoute);
