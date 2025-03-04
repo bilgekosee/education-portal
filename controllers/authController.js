961991a8c6d8862ef28adc45f4604c66c950e1d5
bilgekosee <bilge.koosee@gmail.com>
2025-03-04 21:54:52 +0300

    login added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   try {
@@ -8,6 +9,27 @@ exports.createUser = async (req, res) => {
       status: "success",
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    const same = bcrypt.compare(password, user.password);
+    if (!same) {
+      return res.status(401).send("Invalid password");
+    }
+    return res.status(200).send("YOU ARE LOGGED IN");
   } catch (error) {
     res.status(400).json({
       status: "fail",
