ecdec5686ea59048ee96a548a58f3fc8e62ea04f
bilgekosee <bilge.koosee@gmail.com>
2025-03-09 23:51:13 +0300

    admin eklendi1

--- controllers/authController.js
+++ controllers/authController.js
@@ -59,10 +59,26 @@ exports.dashboardPage = async (req, res) => {
   );
   const categories = await Category.find();
   const courses = await Course.find({ user: req.session.userID });
+  const users = await User.find();
   res.status(200).render("dashboard", {
     page_name: "dashboard",
     user,
     categories,
     courses,
+    users,
   });
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    await Course.deleteMany({ user: req.params.id });
+    res.status(200).redirect("/users/dashboard");
+  } catch (error) {
+    console.error("Error deleting course:", error);
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
 };
